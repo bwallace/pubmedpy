52456773a770d20ec89012c781cd4c23a5038ce0
Tom Trikalinos <tom@ttrikalin-mac.local>
2010-05-01 14:35:13 -0400

    fixed example.py

--- isi/example.py
+++ isi/example.py
@@ -1,14 +1,13 @@
 from my_functions import * 
 
-my_search = 'Nomogram can help estimate risk of serious hyperbilirubinemia in healthy infants'
-pmids = get_pmid(my_search, 2000)
+#my_search = 'Nomogram can help estimate risk of serious hyperbilirubinemia in healthy infants'
+#pmids = get_pmid(my_search, 2000)
 
 titles = read_isi_file("test.txt")
 
+
+f = open("pmids.pickle", "wb+")
 ids = fetch_pmids_from_titles(titles)
-
-f = open("pmids.pickle", "rb+")
 pickle.dump(ids,f)
-
 f.close()
 
